928ef63f6a446eacb40e31b33462be81809b6517
Ingrid Ferreira <gridyfs@gmail.com>
2026-06-06 23:21:16 -0300

    Corrige pagina de envio e edicao de palpites3

--- pages/4_enviarPalpites.py
+++ pages/4_enviarPalpites.py
@@ -160,10 +160,7 @@ else:
     prazo_aberto = rodada_ainda_aberta(
         rodada_aberta
     )
-    st.write(
-        "Prazo aberto:",
-        prazo_aberto
-    )
+    
     if not prazo_aberto:
 
         st.warning(
